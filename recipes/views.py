76efd553df5c709168d0ce47c8c2c4c43014feff
Ubirajara Nunes Junior <unjunior4315@gmail.com>
2022-03-14 23:15:39 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,11 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context= {
-        'name': 'Ubirajara'
+    return render(request, 'recipes/home.html', context={
+        'name': 'Ubirajara Nunes Junior'
     })
 
 
-def contato(request):
-    return render(request, 'recipes/contato.html')
 
 
-def sobre(request):
-    return HttpResponse('sobre')
